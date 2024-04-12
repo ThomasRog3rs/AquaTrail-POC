0464b6b5a86fe9b8416cc93bad0dffd02541518b
Thomas Rogers <Mr.Thomas.Rogers@Proton.me>
2024-04-12 21:04:23 +0100

    set up mapbox

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
+const props = defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ props.msg }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
@@ -30,9 +30,3 @@ const count = ref(0)
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
---
+++ vue/src/components/MapComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div ref="map" class="map-container" id="map-container"></div>
+</template> 
+  
+<script setup lang="ts">
+import {ref, onMounted, onUnmounted} from 'vue';
+import mapboxgl, {Map} from 'mapbox-gl';
+
+mapboxgl.accessToken = '';
+const map = ref<Map|null>(null);
+
+onMounted(() => {
+    if(map.value){
+        map.value = new mapboxgl.Map({
+            container: map.value as HTMLElement,
+            style: 'mapbox://styles/mapbox/streets-v11',
+        });
+    }
+});
+
+onUnmounted(() => {
+  // Clean up when the component is unmounted
+  if (map.value) {
+    map.value.remove();
+  }
+});
+</script>
+
+<style scoped>
+.map-container {
+  width: 700px;
+  height: 700px;
+}
+</style>
