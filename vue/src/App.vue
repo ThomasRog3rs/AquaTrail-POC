ce7aa8ddde5e02751c346b5901528656bc05e3da
Thomas Rogers <Mr.Thomas.Rogers@Proton.me>
2024-06-23 02:26:03 +0100

    add loading annimation

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div v-if="!mapStore.mapLoaded" class="overlay">
-    Loading..
+    <h1>Loading...</h1>
+    <div class="loading">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
   </div>
 
   <template v-if="mapStore.mapLoaded">
@@ -8,11 +13,12 @@
     <SideNav></SideNav>
   </template>
 
-  <main class="">
+  <main>
     <div class="flex-1 min-h-0 h-[calc(100vh-69px)] w-full ml-0 sm:ml-[25rem] sm:w-[calc(100vw-25rem)]">
       <MapComponent></MapComponent>
     </div>
   </main>
+
 </template>
 
 <script setup lang="ts">
@@ -24,18 +30,99 @@ const mapStore = useMapStore();
 </script>
 
 <style>
-.content {
-    flex: 1;
-    min-height: 0;
-    height: calc(100vh - 69px); /* 100vh minus the height of the nav */
-    width: calc(100vw - 25rem); /* 100vw minus the width of the side nav */
-    margin-left: 25rem;
-}
-
 .overlay{
   width: 100vw;
   height: 100vh;
-  background-color: aqua;
-  /* z-index: 999999; */
+  background-color: rgb(234, 234, 234);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  overflow: none;
 }
+
+.loading {
+  position: relative;
+  width: 110px;
+  display: flex;
+  align-items: center;
+  margin-top: 50px; /* Adjust margin-top to create space between h1 and loading */
+}
+
+.loading span {
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    background-color: rgb(22 163 74);
+    border-radius: 30px;
+    position: absolute;
+}
+
+.loading span:first-child {
+    animation: loading-span1 1.3s infinite;
+    left: 0;
+}
+
+.loading span:nth-child(2) {
+    animation: loading-span2 1.3s infinite;
+    left: 40px;
+}
+
+.loading span:nth-child(3){
+    animation: loading-span3 1.3s infinite;
+    left: 80px;
+}
+
+@keyframes loading-span1 {
+    0% {
+        height: 30px;
+    }
+    25% {
+        height: 60px;
+    }
+    50% {
+        height: 30px;
+    }
+    100% {
+        height: 30px;
+    }
+}
+
+@keyframes loading-span2 {
+    0% {
+        height: 30px;
+    }
+    25% {
+        height: 30px;
+    }
+    50% {
+        height: 60px;
+    }
+    75% {
+        height: 30px;
+    }
+    100% {
+        height: 30px;
+    }
+}
+
+@keyframes loading-span3 {
+    0% {
+        height: 30px;
+    }
+    25% {
+        height: 30px;
+    }
+    50% {
+        height: 30px;
+    }
+    75% {
+        height: 60px;
+    }
+    100% {
+        height: 30px;
+    }
+}
+
 </style>
