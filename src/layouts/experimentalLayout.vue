8562c6701d67caff2f19b654fcbd24d7ba467bd7
Thomas Rogers <Mr.Thomas.Rogers@Proton.me>
2024-08-16 23:20:48 +0100

    small change to footer

--- src/layouts/experimentalLayout.vue
+++ src/layouts/experimentalLayout.vue
@@ -68,7 +68,7 @@
             <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                 <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Mooring Pin</span>
             </a>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-white">
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium sm:mb-0 text-white">
                 <li>
                   <RouterLink to="/old" class="hover:underline me-4 md:me-6">View Legacy </RouterLink>
                 </li>
