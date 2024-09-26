9eac725ef509a3fc89a9d4fedb157d863509df03
Thomas Rogers <Mr.Thomas.Rogers@Proton.me>
2024-09-26 22:58:09 +0100

    misc

--- src/layouts/experimentalLayout.vue
+++ src/layouts/experimentalLayout.vue
@@ -1,8 +1,8 @@
 <template>
   <nav class="bg-blue-700" style="padding: 0px; padding-top: 10px; padding-bottom: 10px">
     <div class="logo" style="display: flex; justify-content: center; align-items: center;">
-      <img src="../assets/logo4.png" width="70"  alt="Logo" style="margin-right: 0px;">
-      <h1 style="color: white; font-size: 24px; margin: 0;">Mooring Pin</h1>
+      <img src="../assets/logo4.png" width="70"  alt="Logo" style="margin-right: -4px;">
+      <h1 style="color: white; font-size: 25px; margin: 0;" class="font-bold">Mooring Pin</h1>
     </div>
   </nav>
       <section id="search">
