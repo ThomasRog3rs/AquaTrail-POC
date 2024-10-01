32cd616d15f0f558d9e2fc905892bb0439326720
Thomas Rogers <Mr.Thomas.Rogers@Proton.me>
2024-10-01 01:42:26 +0100

    website link text does not need to be the url

--- src/pages/Marina.vue
+++ src/pages/Marina.vue
@@ -64,7 +64,12 @@
 
               <template v-if="marina?.website != undefined">
                 <h4 class="mt-2 mr-2 font-medium">Website:</h4>
-                <a v-if="marina?.website != undefined" :href="marina?.website" target="_blank" class="underline text-blue-600">{{ marina?.website }}</a>
+                <a v-if="marina?.website != undefined" :href="marina?.website" target="_blank" class="underline text-blue-600">
+                  <svg class="w-6 h-6 underline" style="display: inline-block" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.213 9.787a3.391 3.391 0 0 0-4.795 0l-3.425 3.426a3.39 3.39 0 0 0 4.795 4.794l.321-.304m-.321-4.49a3.39 3.39 0 0 0 4.795 0l3.424-3.426a3.39 3.39 0 0 0-4.794-4.795l-1.028.961"/>
+</svg>Website 
+
+                </a>
               </template>
             </div>
             <div id="marina-notes" class="mb-2 p-4 border-4 rounded-lg" v-if="marina?.phoneNumber != undefined">
